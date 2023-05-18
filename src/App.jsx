e643f4008eafa62a59adebdec439772196c76bf2
wangkodok <wangkodok@gmail.com>
2023-05-18 16:29:45 +0900

    리액트 CRA 없이 설치

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "./Title";
+
+export default function App() {
+  return (
+    <section>
+      <Title />
+    </section>
+  );
+}
---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title() {
+  return <h1>리액트 CRA 없이 설치</h1>;
+}
+
+export default Title;
